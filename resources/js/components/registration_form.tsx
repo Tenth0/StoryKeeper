c945ff11719d0f5fc12f3916ec5feae5582b7439
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-27 00:30:16 +0900

    登録フォームを作った

---
+++ resources/js/components/registration_form.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import InputGroup from "react-bootstrap/InputGroup";
+
+const RegistrationForm = () => {
+    return (
+        <>
+            <Form>
+                <Form.Group as={Row} className="mb-3" controlId="title">
+                    <Form.Label column sm="2">
+                        ファイル
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="file" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="title">
+                    <Form.Label column sm="2">
+                        タイトル
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control
+                            type="text"
+                            placeholder="タイトルを入力してください"
+                        />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="category">
+                    <Form.Label column sm="2">
+                        カテゴリ－
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Select aria-label="Default select example">
+                            <option key="0">カテゴリー</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="comment">
+                    <Form.Label column sm="2">
+                        読んだ日
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="date" />
+                    </Col>
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                >
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Form>
+        </>
+    );
+};
+
+export default RegistrationForm;
--- resources/js/pages/add_item.tsx
+++ resources/js/pages/add_item.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavigationBar from '@/components/navigation_bar'
+import RegistrationForm from '@/components/registration_form'
 
 const AddItem:React.FC = () => {
   return (
     <>
         <NavigationBar />
-        <div>AddItem</div>
+        <RegistrationForm />
     </>
   )
 }
