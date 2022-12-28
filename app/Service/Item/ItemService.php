585e34453bb030f095af3f20fc98df50ce0962ab
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-28 23:21:12 +0900

    CategoryとItemの仮サービス作成

--- app/Service/Item/ItemService.php
+++ app/Service/Item/ItemService.php
@@ -1,2 +1,34 @@
 <?php
 
+namespace App\Service\Category;
+
+use App\Models\Item;
+use App\Models\Category;
+use App\Repository\Item\ItemRepositoryInterface;
+use App\Repository\Category\CategoryRepositoryInterface;
+use App\Utils;
+use Illuminate\Support\Facades\DB;
+use PDO;
+use PDOException;
+use Log;
+
+
+class CategoryService implements CategoryServiceInterface {
+
+    private $ItemRepo;
+
+    private $CategoryRepo;
+
+    public const NOT_USE_FILED = [
+        'id',
+    ];
+
+    public function __construct(
+        ItemRepositoryInterface $ItemRepo,
+        CategoryRepositoryInterface $CategoryRepo
+    )
+    {
+        $this->ItemRepo = $ItemRepo;
+        $this->CategoryRepo = $CategoryRepo;
+    }
+}
