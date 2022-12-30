e2860bd2dbe3c88b99d2632029dad9ab4e11d3b5
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-30 22:19:23 +0900

    ナビゲーションバーの改善

--- resources/js/components/navigation_bar.tsx
+++ resources/js/components/navigation_bar.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 const NavigationBar = () => {
   return (
     <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand href="/">Navbar</Navbar.Brand>
+      <Navbar.Brand href="/"><BsFillMoonStarsFill /></Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="/">一覧画面</Nav.Link>
-        <Nav.Link href="/insert_item">タイトル追加</Nav.Link>
-        <Nav.Link href="/category_table">カテゴリー追加</Nav.Link>
+        <Nav.Link href="/">アイテム一覧</Nav.Link>
+        <Nav.Link href="/insert_item">アイテム編集</Nav.Link>
+        <Nav.Link href="/category_table">カテゴリー編集</Nav.Link>
       </Nav>
     </Container>
   </Navbar>
