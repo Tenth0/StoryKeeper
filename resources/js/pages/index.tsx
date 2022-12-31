445b293beeaffb53f3ed7dfd472f195aa986e527
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2022-12-31 21:32:30 +0900

    atomにアイテムを登録する

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavigationBar from '@/components/navigation_bar';
 import HomeBody from '@/components/Home/home_body';
 import HomeFooter from '@/components/Home/home_footer';
-import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
+import { itemsState } from '@/states/items';
 
 const index:React.FC = (props) => {
   const { items }:any = props
   console.log(items)
-  
+  const setItems = useSetRecoilState(itemsState);
+  setItems(items)
   return (
     <>
       <NavigationBar />
