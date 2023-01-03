e0386c617458a98634ace88f59b28f0b28bfd31a
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-04 00:54:21 +0900

    カテゴリーのデータをもでるから取得する

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -1,22 +1,29 @@
-import React,{useEffect} from 'react'
-import NavigationBar from '@/components/navigation_bar';
-import HomeBody from '@/components/Home/home_body';
-import HomeFooter from '@/components/Home/home_footer';
-import { useSetRecoilState } from 'recoil';
-import { itemsState } from '@/states/items';
+import React, { useEffect } from "react";
+import NavigationBar from "@/components/navigation_bar";
+import HomeBody from "@/components/Home/home_body";
+import HomeFooter from "@/components/Home/home_footer";
+import { useSetRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
+import { itemsState } from "@/states/items";
+import { categoriesState } from "@/states/categories";
 
-const index:React.FC = ({ items }:any) => {
-  const setItems = useSetRecoilState(itemsState);
-  useEffect(() => {
-    setItems(items)
-  }, [])
-  return (
-    <>
-      <NavigationBar />
-      <HomeBody />
-      <HomeFooter />
-    </>
-  );
-}
+const Index: React.FC<{}> = ({ items, categories }: any) => {
+    // 型
+    const setItems = useSetRecoilState(itemsState);
+    const setCategories = useSetRecoilState(categoriesState);
+    useEffect(() => {
+        setItems(items);
+        setCategories(categories);
+    }, [items,categories]);
+    const categoryList = useRecoilValue(categoriesState);
+    console.log(categoryList);
+    return (
+        <>
+            <NavigationBar />
+            <HomeBody />
+            <HomeFooter />
+        </>
+    );
+};
 
-export default index
+export default Index;
