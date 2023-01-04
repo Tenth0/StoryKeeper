66b7098fb2c31cb4ea4f67b2b9254eb8e35a549b
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-04 23:51:51 +0900

    検索と全体を取得するAPIを分けた

--- resources/js/components/Home/input_form.tsx
+++ resources/js/components/Home/input_form.tsx
@@ -1,50 +1,69 @@
-import Col from 'react-bootstrap/Col';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import { useRecoilValue } from "recoil";
+import Col from "react-bootstrap/Col";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import { useSetRecoilState, useRecoilValue } from 'recoil';
 import { categoriesState } from "@/states/categories";
+import { itemsState } from "@/states/items";
 import { Category } from "@/types";
-import axios from 'axios';
+import axios from "axios";
 
 const InputForm: React.FC = () => {
-  const categories = useRecoilValue(categoriesState);
-  if (!Array.isArray(categories)) {
-      return null;
-  }
+    const categories = useRecoilValue(categoriesState);
+    if (!Array.isArray(categories)) {
+        return null;
+    }
+    
+    const searchRecord = () => {
+      const titleKeyword: string =
+      document.getElementById("title_keyword")!.value;
+      const selectCategory: string =
+      document.getElementById("select_category")!.value;
+      const selectCategoryId = selectCategory ? parseInt(selectCategory) : "";
+      axios.get(
+        "/search?title_keyword=" +
+        titleKeyword +
+        "&select_category=" +
+        selectCategoryId
+        ).then((items) => {
+          const setItems = useSetRecoilState(itemsState);
+          console.log(items);
+          setItems(items)
+        });
+    };
 
-  const searchRecord = () => {
-    const titleKeyword: string = document.getElementById('title_keyword')!.value;
-    const selectCategory: string = document.getElementById('select_category')!.value;
-    console.log(selectCategory)
-    const selectCategoryId = (selectCategory) ? parseInt(selectCategory) : "";
-    console.log(selectCategoryId)
-    axios.get('/?title_keyword=' + titleKeyword + '&select_category=' + selectCategoryId)
-  }
-  
-
-  return (
-    <Row className="g-2">
-      <Col md>
-        <FloatingLabel label="タイトル">
-          <Form.Control id="title_keyword" type="text" placeholder="例:ノーゲーム・ノーライフ、キングダム" onBlur={() => searchRecord()}/>
-        </FloatingLabel>
-      </Col>
-      <Col md>
-        <FloatingLabel
-          controlId="floatingSelectGrid"
-          label="カテゴリー"
-        >
-          <Form.Select id="select_category" onChange={() => searchRecord()}>
-            <option></option>
-            {categories.map((category: Category) => (
-              <option key={category.id} value={category.id}>{category.title}</option>
-            ))}
-          </Form.Select>
-        </FloatingLabel>
-      </Col>
-    </Row>
-  );
-}
+    return (
+        <Row className="g-2">
+            <Col md>
+                <FloatingLabel label="タイトル">
+                    <Form.Control
+                        id="title_keyword"
+                        type="text"
+                        placeholder="例:ノーゲーム・ノーライフ、キングダム"
+                        onBlur={() => searchRecord()}
+                    />
+                </FloatingLabel>
+            </Col>
+            <Col md>
+                <FloatingLabel
+                    controlId="floatingSelectGrid"
+                    label="カテゴリー"
+                >
+                    <Form.Select
+                        id="select_category"
+                        onChange={() => searchRecord()}
+                    >
+                        <option></option>
+                        {categories.map((category: Category) => (
+                            <option key={category.id} value={category.id}>
+                                {category.title}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </FloatingLabel>
+            </Col>
+        </Row>
+    );
+};
 
 export default InputForm;
