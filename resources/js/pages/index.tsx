8c4eb386d985d84adfc4862e52d76c5a91d6e023
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-05 23:18:50 +0900

    型

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -4,7 +4,7 @@ import HomeBody from "@/components/Home/home_body";
 import HomeFooter from "@/components/Home/home_footer";
 import { useSetRecoilState } from "recoil";
 import { itemsState } from "@/states/items";
-import { categoriesState } from '@/states/categories';
+import { categoriesState } from "@/states/categories";
 
 const Index: React.FC<{}> = ({ items, categories }: any) => {
     // 型
@@ -13,7 +13,7 @@ const Index: React.FC<{}> = ({ items, categories }: any) => {
     useEffect(() => {
         setItems(items);
         setCategories(categories);
-    }, [items,categories]);
+    }, [items, categories]);
     return (
         <>
             <NavigationBar />
--- resources/js/types.ts
+++ resources/js/types.ts
@@ -13,7 +13,21 @@ export type Category = {
     id: number;
     title: string;
     color: Color;
-  };
-  
-  export type Color = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light';
+};
+
+export type Color = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light';
+
+export type CardData = {
+    id:number,
+    title:string,
+    category_id:number,
+    filename:string,
+    comment:string,
+    read_time:string,
+    order:number,
+    is_favorite:boolean,
+    category_title: string,
+    color: Color,
+}
+
   
