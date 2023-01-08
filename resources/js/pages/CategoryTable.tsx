c1dba4840f2177c4a9fb9f685480b6d66bfa9ea2
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-09 02:39:04 +0900

    ページを再読み込みしても画面が表示されるように変更する

--- resources/js/pages/CategoryTable.tsx
+++ resources/js/pages/CategoryTable.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
 import CategoryBody from '@/components/Category/CategoryBody';
+import { useSetRecoilState } from 'recoil';
+import { categoriesState } from '../states/categories';
+import NavigationBar from '@/components/NavigationBar';
 
-const CategoryTable:React.FC = () => {
+const CategoryTable:React.FC<{}> = ({categories}:any) => {
+  if(categories) {
+    const setCategories = useSetRecoilState(categoriesState);
+    useEffect(() => {
+      setCategories(categories)
+    }, [])
+    return (
+      <>
+        <NavigationBar />
+        <CategoryBody />
+      </>
+    )
+  }
+
   return (
     <>
       <CategoryBody />
