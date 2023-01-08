ea9d9a73bf17167c2a7200cd1a5a44c4a661b1ef
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-08 19:20:55 +0900

    ナビゲーションバーをReact-Routerに変更

--- resources/js/pages/CategoryTable.tsx
+++ resources/js/pages/CategoryTable.tsx
@@ -4,12 +4,7 @@ import CategoryBody from '@/components/Category/CategoryBody';
 import { useSetRecoilState } from 'recoil';
 import { categoriesState } from '../states/categories';
 
-const CategoryTable:React.FC<{}> = ({categories}:any) => {
-  const setCategories = useSetRecoilState(categoriesState);
-  useEffect(() => {
-    setCategories(categories)
-  }, [])
-
+const CategoryTable:React.FC<{}> = () => {
   return (
     <>
       <NavigationBar />
