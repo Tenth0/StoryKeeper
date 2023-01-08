bab71d7f53dc2ecbf3f17d9c5c29629f86d20bcd
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-09 02:41:01 +0900

    アイテム追加画面もリロードでよみこまれるようにした

--- resources/js/pages/InsertItem.tsx
+++ resources/js/pages/InsertItem.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import InsertItemBody from "@/components/Item/InsertItemBody";
-const InsertItem: React.FC = () => {
+import { useSetRecoilState } from "recoil";
+import { categoriesState } from "../states/categories";
+import { Category } from '../types';
+import NavigationBar from "@/components/NavigationBar";
+
+const InsertItem: React.FC<{}> = ({ categories }:any) => {  
+    if(categories) {
+        const setCategories = useSetRecoilState(categoriesState);
+        useEffect(() => {
+          setCategories(categories)
+        }, [categories])
+        return (
+          <>
+            <NavigationBar />
+            <InsertItemBody />
+          </>
+        )
+    }
     return (
         <>
             <InsertItemBody />
