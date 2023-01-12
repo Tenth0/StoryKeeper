033f235c30bdaecadd4d8e87b16e1d8389de1b87
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-12 10:25:25 +0900

    トーストのひな形

---
+++ resources/js/components/Toast/ToastSuccess.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Button, Col, Row, Toast } from "react-bootstrap";
+
+const ToastSuccess = () => {
+    return (
+        <Row>
+            <Col xs={6}>
+                <Toast
+                    delay={2000}
+                >
+                    <Toast.Header>
+                        <strong className="me-auto">Bootstrap</strong>
+                    </Toast.Header>
+                    <Toast.Body>
+                        Woohoo, you're reading this text in a Toast!
+                    </Toast.Body>
+                </Toast>
+            </Col>
+        </Row>
+    );
+};
+
+export default ToastSuccess;
