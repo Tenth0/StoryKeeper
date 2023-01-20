0ace2000395b3dfc929bc52958dfb100612a5c63
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-20 23:00:59 +0900

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     root: process.cwd(),
-    build:{//この部分を追加
-      outDir:"./build"
-    },
     server:{
       host:'0.0.0.0'
     },
