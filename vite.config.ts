b8dde3cdde74e123ed5c867a377bf485e33b7b48
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-23 10:22:22 +0900

    設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    root: process.cwd(),
     server:{
       host:'0.0.0.0'
     },
