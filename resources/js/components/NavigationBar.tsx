c84690d25f24f946cbe8a8d05c84b69a44769c2b
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-01-30 23:49:30 +0900

    ロゴを設定

--- resources/js/components/NavigationBar.tsx
+++ resources/js/components/NavigationBar.tsx
@@ -4,12 +4,17 @@ import { Nav,Navbar } from 'react-bootstrap';
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { LinkContainer } from 'react-router-bootstrap';
 
+const imgStyle = {
+  width: '30%',
+  height: '50%',
+}
+
 const NavigationBar:React.FC = () => {
   return (
     <Navbar bg="dark" variant="dark">
     <Container>
       <LinkContainer to="/">
-        <Navbar.Brand><BsFillMoonStarsFill /></Navbar.Brand>
+        <Navbar.Brand><img src='./images/StoryKeeper.png' style={imgStyle}/></Navbar.Brand>
       </LinkContainer>
       <Nav className="me-auto">
         <LinkContainer to="/">
